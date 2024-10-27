047538724c69ef4262111548e93dff5b371bab8b
Tanim <2311408@brunel.ac.uk>
2024-10-27 21:25:47 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Menu from './Components/Menu'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello World
+        </p>
+
+         <Menu></Menu> 
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
