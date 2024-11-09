4034108e9a18fe26c43c08426064c332db2ab4c7
Tanim <2311408@brunel.ac.uk>
2024-11-09 11:18:05 +0000

    slideshow after purple line

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Officiallogo from "./Assets/Officiallogo.jpg";
 import Header from "./Components/Header";
 import { FaFacebook, FaInstagram } from 'react-icons/fa';
 import { MdEmail, MdPhone } from 'react-icons/md';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import default styles for carousel
 
 
 function App() {
@@ -33,11 +35,6 @@ function App() {
           </a>
         </div>
 
-
-
-
-
-        
       </header>
 
 
@@ -55,22 +52,45 @@ function App() {
         <Link className="nav-link" to="/Terms"></Link>
       </nav>
 
+
       {/* Main Content */}
       <main className="App-main">
         <Outlet /> {/* Outlet will render the child routes */}
       </main>
+    
       {/* Body of the app */}
-    <div className="App-body">
+      <div className="App-body">
+        {/* Slideshow Section */}
+        <div className="slideshow-container">
+          <Carousel 
+            autoPlay 
+            infiniteLoop 
+            showThumbs={false} 
+            showStatus={false}
+            dynamicHeight={true}
+            className="carousel"
+          >
+            {/* Add your images inside Carousel */}
+            <div>
+              <img src="/path/to/image1.jpg" alt="Slide 1" />
+            </div>
+            <div>
+              <img src="/path/to/image2.jpg" alt="Slide 2" />
+            </div>
+            <div>
+              <img src="/path/to/image3.jpg" alt="Slide 3" />
+            </div>
+            {/* Add more images as needed */}
+          </Carousel>
+        </div>
+      </div>
 
-    </div>
-            {/* Bottom Navigation */}
-
+      {/* Bottom Navigation */}
       <div className="App-bottom-nav">
         <Link className="nav-link" to="/privacy">Privacy Policy</Link>
         <Link className="nav-link" to="/terms">Terms of Use</Link>
         <Link className="nav-link" to="/faq">FAQ</Link>
       </div>
-    
     </div>
   );
 }
